28aecc443d833da7e04e063e8eb0cfa167b1bd64
Nima <165915862+NimaWebDev@users.noreply.github.com>
2025-06-07 06:43:06 -0700

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -25,9 +25,9 @@ import Blogs from '../parts/blogs'
             <h1 className='animate__animated animate__backInDown text-center text-2xl sm:text-4xl font-bold text-[#081035]'>! سفرت رو بچین، <span className='text-[#6DA975]'>وسایلش</span> رو داریم</h1>
             <button onClick={handleProducts} className='animate__animated animate__backInUp w-[170px] h-[44px] bg-[#081035] text-white font-bold rounded-4xl flex items-center gap-1 cursor-pointer ml-auto mr-auto mt-10'><img className='pl-2' src={logoFlash} alt="logo" />مشاهده محصولات</button>
             <div className='container flex max-w-full items-center justify-center'>
-                <img className='hidden lg:block w-[237px] h-[150px] absolute mr-220 mb-50' src={imageLeft} alt="image" />
+                <img className='hidden lg:block h-[150px] absolute mr-220 mb-50' src={imageLeft} alt="image" />
                 <img className=' w-screen h-[300px] sm:w-[1126px] sm:h-[500px] ml-20 mt-20 drop-shadow-[0_16px_16px_rgba(0,0,0,1)]' src={imageHome} alt="image" />
-                <img className='hidden lg:block w-[237px] h-[150px] absolute lg:ml-190 mt-20' src={imageRight} alt="image" />
+                <img className='hidden lg:block h-[150px] absolute lg:ml-190 mt-20' src={imageRight} alt="image" />
             </div>
             <svg className="absolute bottom-0 left-0 w-full h-[40px] " viewBox="0 0 100 40" preserveAspectRatio="none">
                 <path d="M0 0 L45 35 L50 40 L55 35 L100 0" stroke="black" strokeWidth="1" fill="none" />
